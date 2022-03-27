b012d9dacf50ca6f3fc32dd99b51bb9a9f32d787
Cascaes0 <isaquecascaes15@gmail.com>
2022-03-27 17:07:26 -0300

    EXPRESS BY ISAQUE

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-var q = 10
+
+const express = require('express')
+const app = express()
+const port = 30000
+
+app.get('/', (req, res) => {
+  res.send('Testando URL, by Isaque')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
