2f03c362df177e2b71fe6e789fd180ed5b35f620
juvigh <juliolk.4321@gmail.com>
2022-03-27 17:32:22 -0300

    /contato adicionado

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const app = express()
-const port = 30000
+const port = 3200
 
 app.get('/', (req, res) => {
   res.send('Testando URL, by Isaque')
@@ -14,3 +14,9 @@ app.get('/sobre', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+app.get('/contato', (req, res) => {
+  res.send("Nos encontrem em:" +
+  "email:" +
+  "numero:")
+});
