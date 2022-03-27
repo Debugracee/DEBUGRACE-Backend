e75f81652690fe1cfdd2a977c1069ad975d663e6
MEL-49 <gladillis.49@gmail.com>
2022-03-27 17:19:31 -0300

    /sobre adicionado

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('Testando URL, by Isaque')
 })
 
+app.get('/sobre', (req, res) => {
+  res.send('Que somos')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
