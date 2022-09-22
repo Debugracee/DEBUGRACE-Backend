7c17789556eb66e24f2a7b89ee4afbdca6873211
juvigh <juliolk.4321@gmail.com>
2022-09-22 01:49:34 -0300

    criando controller delete/put model trilhas

---
+++ controllers/trilhas/deleteTrilhas.js
@@ -0,0 +1,14 @@
+const deleteTrilhas = async (req, res) => {
+  let trilha = require("../../model/trilhas");
+  const id = req.params.id;
+  const trilhas = await trilha.findByPk(id);
+  const deleteTrilha = await trilha.destroy({
+    where: {
+      id: id,
+    },
+  });
+
+  return res.json({ msg: "trilha deletada", trilhaDeletada: deleteTrilha });
+};
+
+module.exports = deleteTrilhas;
