740248cc663d8577d891227a6af639680f996bcb
juvigh <juliolk.4321@gmail.com>
2022-09-28 12:26:31 -0300

    create middleware 1/3

---
+++ controllers/Middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const auth = async (req, res, next) => {
+  const authHeader = req.headers.authorization
+  const token = authHeader && authHeader.split("  ")[1];
+  if (!token) {
+    return res.status(401).json({ msg: "acesso negado" });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+    jwt.verify(token, secret);
+    next();
+  } catch (error) {
+    return res.status(400).json({ msg: "token invalido" });
+  }
+};
+
+module.exports = auth;
