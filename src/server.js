513558ba07298417104a08f5104c7e7226df2545
juvigh <juliolk.4321@gmail.com>
2022-11-09 22:58:49 -0300

    subir api render

--- src/server.js
+++ src/server.js
@@ -5,19 +5,22 @@ const app = express();
 const sequelize = require("sequelize");
 const usuarioRoute = require("../routers/usuario.routes");
 const trilhasRoutes = require("../routers/trilhas.routes");
-const port = 3500;
+const port =  process.env.DATABASE_PORT || 3500;
 
 app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
-  // res.header("Authorization")
   next();
 });
 app.use(express.json());
 app.use(usuarioRoute);
 app.use(trilhasRoutes);
+
+app.get("/", (req, res) => {
+  res.send('Sua api carregou, amou?' + 'e seu banco é:' + process.env.DATABASE_NAME)
+})
 
 app.listen(port, () => {
   console.log(`Rodando na porta: ${port}`);
